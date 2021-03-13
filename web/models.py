11401e7b78d4cc3105fabaa3d8a4f222cd1d1dfe
María González Moreno <mgm9211@gmail.com>
2021-03-13 21:29:06 +0100

    creado modelos de dispositivo e informacion

--- web/models.py
+++ web/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
 # Create your models here.
+
+
+# class AuthUser(models.Model):
+#     password = models.CharField(max_length=128)
+#     last_login = models.DateTimeField(blank=True, null=True)
+#     is_superuser = models.IntegerField()
+#     username = models.CharField(unique=True, max_length=150)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=150)
+#     email = models.CharField(max_length=254)
+#     is_staff = models.IntegerField()
+#     is_active = models.IntegerField()
+#     date_joined = models.DateTimeField()
+
+
+class Device(models.Model):
+    # user = models.ForeignKey(AuthUser, models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=8, blank=True, null=False, unique=True)
+    ip = models.CharField(max_length=16, blank=True, null=True)
+    visible = models.BooleanField()
+
+
+class Information(models.Model):
+    device = models.ForeignKey(Device, models.DO_NOTHING, blank=True, null=True)
+    temperature = models.CharField(max_length=8, blank=True, null=True, unique=True)
+    humidity = models.CharField(max_length=4, blank=True, null=True, unique=True)
+    created_at = models.DateTimeField()
